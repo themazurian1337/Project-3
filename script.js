4b97f5ff1376570827bef6fa95d194d5f804041d
themazurian <n10447415@qut.edu.au>
2024-01-10 20:17:18 +1000

    Initial function added

--- script.js
+++ script.js
@@ -1,2 +1,25 @@
 let movies = [];
 let isSorted = false;
+
+const addMovie = () => {
+  let movieTitle = document.querySelector('#movieTitle').value.trim();
+  let movieRating = document.querySelector('#movieRating').value;
+  if (movieRating < 0 || movieRating > 5) {
+    alert('Invalid Rating Provided');
+  } else if (movieTitle.length <= 0) {
+    alert('Invalid Title Provided');
+  } else {
+    let movieObject = {
+      title: movieTitle,
+      rating: movieRating,
+    };
+
+    movies.push(movieObject);
+
+    console.log(movies);
+  }
+};
+
+let addMovieButton = document
+  .querySelector('#addMovieButton')
+  .addEventListener('click', addMovie);
