6e6e8eb183a0fbcea59efca94f80a18856f65b98
themazurian <n10447415@qut.edu.au>
2024-01-13 21:22:40 +1000

    Mostly finished project

--- script.js
+++ script.js
@@ -1,12 +1,30 @@
 let movies = [];
-let isSorted = false;
+
+let sortedAccending = true;
+
+const displayMovies = () => {
+  moviesContainer = document.querySelector('#moviesDisplay');
+  moviesContainer.innerHTML = '';
+
+  if (!movies.length) {
+    let emptyListing = document.createElement('li');
+    emptyListing.textContent = 'There are currently no movies to display';
+    moviesContainer.appendChild(emptyListing);
+  } else {
+    movies.forEach((movie) => {
+      let newMovieListing = document.createElement('li');
+      newMovieListing.textContent = `${movie.title} - Rating: ${movie.rating}`;
+      moviesContainer.appendChild(newMovieListing);
+    });
+  }
+};
 
 const addMovie = () => {
   let movieTitle = document.querySelector('#movieTitle').value.trim();
-  let movieRating = document.querySelector('#movieRating').value;
-  if (movieRating < 0 || movieRating > 5) {
+  let movieRating = parseFloat(document.querySelector('#movieRating').value);
+  if (isNaN(movieRating) || movieRating < 0 || movieRating > 5) {
     alert('Invalid Rating Provided');
-  } else if (movieTitle.length <= 0) {
+  } else if (!movieTitle.length) {
     alert('Invalid Title Provided');
   } else {
     let movieObject = {
@@ -15,11 +33,51 @@ const addMovie = () => {
     };
 
     movies.push(movieObject);
+    document.querySelector('#movieTitle').value = '';
+    document.querySelector('#movieRating').value = '';
 
-    console.log(movies);
+    displayMovies();
   }
 };
 
 let addMovieButton = document
   .querySelector('#addMovieButton')
   .addEventListener('click', addMovie);
+
+const movieSort = () => {
+  switch (sortedAccending) {
+    case true:
+      for (let i = 0; i < movies.length; i++) {
+        for (let j = 0; j < movies.length - i - 1; j++) {
+          if (movies[j].rating < movies[j + 1].rating) {
+            let temp = movies[j];
+            movies[j] = movies[j + 1];
+            movies[j + 1] = temp;
+          }
+        }
+      }
+      sortedAccending = false;
+      break;
+
+    case false:
+      for (let i = 0; i < movies.length; i++) {
+        for (let j = 0; j < movies.length - i - 1; j++) {
+          if (movies[j].rating > movies[j + 1].rating) {
+            let temp = movies[j];
+            movies[j] = movies[j + 1];
+            movies[j + 1] = temp;
+          }
+        }
+      }
+      sortedAccending = true;
+
+      break;
+  }
+
+  displayMovies();
+};
+
+let sortMovieButton = document
+  .querySelector('#sortMovieButton')
+  .addEventListener('click', movieSort);
+displayMovies();
